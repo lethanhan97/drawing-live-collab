392919b12fe52457bea1ef49999b72172ab72421
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-10-23 10:35:22 +0800

    feat: add react router dom

--- packages/frontend/src/components/App/App.tsx
+++ packages/frontend/src/components/App/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import LandingPage from '../LandingPage';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 const AppStyled = styled.section`
   height: 100vh;
@@ -10,7 +16,12 @@ const AppStyled = styled.section`
 function App() {
   return (
     <AppStyled>
-      <LandingPage></LandingPage>
+      <Router>
+        <Switch>
+          <Route path="/login" component={LandingPage} />
+          <Route path="*" render={() => <Redirect to="/login" />} />
+        </Switch>
+      </Router>
     </AppStyled>
   );
 }
