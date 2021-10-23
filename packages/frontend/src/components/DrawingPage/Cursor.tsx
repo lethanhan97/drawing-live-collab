f3daf18ec0812f19182eacc351fdb74d97ce606f
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-10-23 11:13:21 +0800

    feat: add custom cursor component

---
+++ packages/frontend/src/components/DrawingPage/Cursor.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Cursor() {
+  return (
+    <svg width={40} height={40}>
+      <circle fill="red" cx={20} cy={20} r={20}></circle>
+    </svg>
+  );
+}
