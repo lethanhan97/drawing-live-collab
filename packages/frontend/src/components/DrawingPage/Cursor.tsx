7d29c7d37d79422dd89b9053f3ef1b5f583117a0
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-10-24 12:09:13 +0800

    feat: add label to cursor

--- packages/frontend/src/components/DrawingPage/Cursor.tsx
+++ packages/frontend/src/components/DrawingPage/Cursor.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
-export default function Cursor() {
+export interface CursorProps {
+  shouldDisplay: boolean;
+  x: number;
+  y: number;
+}
+
+export default function Cursor({ x, y, shouldDisplay }: CursorProps) {
+  const CURSOR_RADIUS = 5;
+  const CURSOR_LABEL_PADDING = 20;
+  const SVG_WIDTH = 200;
+  const SVG_HEIGHT = CURSOR_RADIUS + 40;
+
   return (
-    <svg width={40} height={40}>
-      <circle fill="red" cx={20} cy={20} r={20}></circle>
+    <svg
+      width={SVG_WIDTH}
+      height={SVG_HEIGHT}
+      fill="red"
+      style={{
+        position: 'absolute',
+        left: x - SVG_WIDTH / 2,
+        top: y - CURSOR_RADIUS,
+        pointerEvents: 'none',
+        visibility: shouldDisplay ? 'visible' : 'hidden',
+      }}
+    >
+      <g>
+        <circle cx={SVG_WIDTH / 2} cy={CURSOR_RADIUS} r={CURSOR_RADIUS} />
+        <text
+          x="50%"
+          y={CURSOR_RADIUS * 2 + CURSOR_LABEL_PADDING}
+          textAnchor="middle"
+        >
+          An
+        </text>
+      </g>
     </svg>
   );
 }
--- packages/frontend/src/components/DrawingPage/DrawingPage.tsx
+++ packages/frontend/src/components/DrawingPage/DrawingPage.tsx
@@ -1,8 +1,18 @@
-import React, { useEffect, useRef } from 'react';
-import { finalize, fromEvent, switchMap, takeUntil, tap } from 'rxjs';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  finalize,
+  fromEvent,
+  map,
+  merge,
+  Observable,
+  switchMap,
+  takeUntil,
+  tap,
+} from 'rxjs';
 import styled from 'styled-components';
 import { paperShadowOuter } from '../../shared/style/box-shadow';
 import { colorCodes } from '../../shared/style/colors';
+import Cursor, { CursorProps } from './Cursor';
 
 const DrawingPageStyled = styled.div`
   background-color: ${colorCodes.lightMustard};
@@ -15,7 +25,13 @@ const DrawingPageStyled = styled.div`
 const CanvasStyled = styled.canvas`
   background-color: white;
   box-shadow: ${paperShadowOuter};
+  /* cursor: none; */
 `;
+
+interface CursorCustomEvent {
+  eventType: 'mouseMove' | 'mouseLeave';
+  eventPayload: MouseEvent;
+}
 
 function DrawableCanvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -36,7 +52,6 @@ function DrawableCanvas() {
     const subscription = mouseDown$
       .pipe(
         tap(({ offsetX, offsetY }) => {
-          console.log('mousedown');
           context?.beginPath();
           context?.moveTo(offsetX, offsetY);
         }),
@@ -48,7 +63,6 @@ function DrawableCanvas() {
           )
         ),
         tap(({ offsetX, offsetY }) => {
-          console.log('mouse moving');
           context?.lineTo(offsetX, offsetY);
           context?.stroke();
         })
@@ -58,8 +72,60 @@ function DrawableCanvas() {
     return () => subscription.unsubscribe();
   }, [canvasRef]);
 
+  const [cursorDisplayState, setCursorDisplayState] = useState<CursorProps>({
+    shouldDisplay: false,
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const { current: canvasEl } = canvasRef;
+
+    const mouseMove$: Observable<CursorCustomEvent> = fromEvent<MouseEvent>(
+      canvasEl,
+      'mousemove'
+    ).pipe(map((eventPayload) => ({ eventType: 'mouseMove', eventPayload })));
+
+    const mouseLeave$: Observable<CursorCustomEvent> = fromEvent<MouseEvent>(
+      canvasEl,
+      'mouseleave'
+    ).pipe(map((eventPayload) => ({ eventType: 'mouseLeave', eventPayload })));
+
+    const subscription = merge(mouseMove$, mouseLeave$)
+      .pipe(
+        tap(({ eventType, eventPayload }) => {
+          if (eventType === 'mouseLeave') {
+            setCursorDisplayState((prevState) => ({
+              ...prevState,
+              shouldDisplay: false,
+            }));
+          } else {
+            const { clientX: x, clientY: y } = eventPayload;
+            setCursorDisplayState({
+              shouldDisplay: true,
+              x,
+              y,
+            });
+          }
+        })
+      )
+      .subscribe();
+
+    return () => subscription.unsubscribe();
+  }, [canvasRef]);
+
+  const { x, y, shouldDisplay } = cursorDisplayState;
   return (
-    <CanvasStyled ref={canvasRef} width={CANVAS_WIDTH} height={CANVAS_HEIGHT} />
+    <>
+      <Cursor x={x} y={y} shouldDisplay={shouldDisplay} />
+      <CanvasStyled
+        ref={canvasRef}
+        width={CANVAS_WIDTH}
+        height={CANVAS_HEIGHT}
+      />
+    </>
   );
 }
 
